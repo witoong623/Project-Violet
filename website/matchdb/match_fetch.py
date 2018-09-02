630951f9aa2c38c7e59beded3c33e35240bd34ba
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-09-02 16:50:31 +0700

    Add Match fetch function to fetch match data from football API

---
+++ website/matchdb/match_fetch.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import dateutil.parser
+from django.conf import settings
+from models import Match, Team
+
+PL_2018_LINK = 'http://api.football-data.org/v2/competitions/2021/matches?season=2018'
+
+def fetch_and_update_match():
+    assert settings.FOOTBALL_API_TOKEN, 'Football API Token is required in environment variable'
+
+    HEADER = { 'X-Auth-Token': settings.FOOTBALL_API_TOKEN }
+    response = requests.get(PL_2018_LINK, headers=HEADER)
+    response.raise_for_status()
+    json = response.json()
+
+    matches = json['matches']
+
+    for json_match in matches:
+        id = json_match['id']
+        status = json_match['status']
+        match_day = json_match['matchday']
+        date = dateutil.parser.parse(json_match['utcDate'])
+
+        match = Match(id=id, status=status, match_day=match_day, date=date)
+
+        home_team_json = json_match['homeTeam']
+        away_team_json = json_match['awayTeam']
+
+        match.home_team = Team.objects.get_or_create(id=home_team_json['id'], defaults={'name': home_team_json['name']})
+        match.away_team = Team.objects.get_or_create(id=away_team_json['id'], defaults={'name': away_team_json['name']})
+
+        match.save()
--- website/models.py
+++ website/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Match(models.Model):
+    POSTPONED = 'POSTPONED'
+    SCHEDULED = 'SCHEDULED'
+    CANCELED = 'CANCELED'
+    SUSPENDED = 'SUSPENDED'
+    IN_PLAY = 'IN_PLAY'
+    PAUSED = 'PAUSED'
+    FINISHED = 'FINISHED'
+    AWARDED = 'AWARDED'
+    MATCH_STATUS = (
+        (POSTPONED, 'Postponed'),
+        (SCHEDULED, 'Scheduled'),
+        (CANCELED, 'Canceled'),
+        (SUSPENDED, 'Suspended'),
+        (IN_PLAY, 'In Play'),
+        (PAUSED, 'Paused'),
+        (FINISHED, 'Finished'),
+        (AWARDED, 'Awarded')
+    )
+
+    HOME_TEAM = 'HOME_TEAM'
+    AWAY_TEAM = 'AWAY_TEAM'
+    WINNER_TEAM = (
+        (HOME_TEAM, 'Home Team'),
+        (AWAY_TEAM, 'Away Team')
+    )
+
+    id = models.IntegerField(primary_key=True)
+    status = models.CharField(max_length=9, choices=MATCH_STATUS, default=SCHEDULED)
+    match_day = models.IntegerField()
+    date = models.DateTimeField()
+
+    home_team = models.ForeignKey(Team, models.CASCADE)
+    away_team = models.ForeignKey(Team, models.CASCADE)
+    
+
+class Team(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+
