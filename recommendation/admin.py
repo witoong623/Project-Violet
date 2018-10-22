fb563699ff39377db4a7fce80d1477ae6c9a2999
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-10-22 22:02:22 +0700

    Change a file to pep8 style

--- recommendation/admin.py
+++ recommendation/admin.py
@@ -9,6 +9,7 @@ from .models import Player
 from .models import MatchProfileValues
 from .models import UserWatchGenerateQueries
 
+
 class UserCaseListFilter(admin.SimpleListFilter):
     title = ('user case')
     parameter_name = 'user_case'
@@ -26,6 +27,7 @@ class UserCaseListFilter(admin.SimpleListFilter):
         if self.value() == 'u11':
             return queryset.filter(Q(ms_team1=19) | Q(ms_team2=19) | Q(ms_team1=1) | Q(ms_team2=1))
 
+
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('p_name', 'active_flag')
     list_filter = ('active_flag', 't_id')
@@ -34,12 +36,15 @@ class PlayerAdmin(admin.ModelAdmin):
     def set_inactive(self, request, queryset):
         queryset.update(active_flag=False)
     set_inactive.short_description = 'Set inactive'
+
     def set_active(self, request, queryset):
         queryset.update(active_flag=True)
     set_active.short_description = 'Set active'
 
+
 class UserWatchGenerateQueriesAdmin(admin.ModelAdmin):
     list_display = ('uwg_id', 'uwg_description')
+
 
 class MatchScheduleAdmin(admin.ModelAdmin):
     list_display = ('ms_id', 'ms_team1', 'ms_team2', 'ms_time')
