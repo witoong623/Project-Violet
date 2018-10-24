45b6ffe3492415509bcf25d567e3c8811c0a5306
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-10-24 15:33:43 +0700

    Add models Competition and Season to website app for better handling match's season and league

--- website/models.py
+++ website/models.py
@@ -46,12 +46,15 @@ class Match(models.Model):
     match_day = models.IntegerField()
     date = models.DateTimeField()
 
-    home_team = models.ForeignKey(Team, models.CASCADE, related_name='home_team')
-    away_team = models.ForeignKey(Team, models.CASCADE, related_name='away_team')
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team')
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_team')
 
     home_score = models.IntegerField(blank=True, null=True)
     away_score = models.IntegerField(blank=True, null=True)
     winner = models.CharField(max_length=9, blank=True)
+
+    competition = models.ForeignKey('Competition', related_name='matches', on_delete=models.CASCADE)
+    season = models.ForeignKey('Season', on_delete=models.CASCADE)
 
     def __str__(self):
         return '{} VS {}'.format(self.home_team, self.away_team)
@@ -63,3 +66,22 @@ class UserWatchHistory(models.Model):
 
     class Meta:
         unique_together = (('user', 'match'),)
+
+
+class Competition(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+
+    currentSeason = models.ForeignKey('Season', blank=True, null=True, related_name='+', on_delete=models.SET_NULL)
+
+
+class Season(models.Model):
+    id = models.IntegerField(primary_key=True)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    currentMatchday = models.IntegerField()
+    display_name = models.CharField(max_length=20, blank=True)
+
+    winner = models.ForeignKey(Match, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
+    competition = models.ForeignKey(Competition, related_name='seasons', on_delete=models.CASCADE)
