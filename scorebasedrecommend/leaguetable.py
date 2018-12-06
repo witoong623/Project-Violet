ee43d0c72d61ee0986f0ae908d363ba3f8de1e70
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-12-06 15:55:42 +0700

    Update leaguetable module to use models from website app

--- scorebasedrecommend/leaguetable.py
+++ scorebasedrecommend/leaguetable.py
@@ -1,61 +1,92 @@
-from recommendation.models import MatchSchedule, Team
-from django.db.models import Q
 from datetime import date
+from django.db.models import Q
+from website.models import Match, Team
+
 
 class LeagueTable:
     score_table = []
     played_count = 0
     teams_count = 0
 
-    def __init__(self, league_name, at_date):
+    def __init__(self, competition_name, season, at_date):
         self.at_date = at_date
-        self.league_name = league_name
+        self.competition_name = competition_name
+        self.season = season
+
 
 class LeagueTableRow:
+    played = 0
     point = 0
-    goals = 0
-    team_name = ''
 
-    def __init__(self, t_id):
-        self.t_id = t_id
+    won = 0
+    drawn = 0
+    lost = 0
+
+    goal_for = 0
+    goal_against = 0
+    goal_difference = 0
+
+    def __init__(self, team_id, team_name):
+        self.team_id = team_id
+        self.team_name = team_name
+
 
 # TODO this method is specifically implement for PL
-def get_score_table(league_name, at_date):
-    st = LeagueTable(league_name, at_date)
+def get_score_table(competition, at_date):
+    st = LeagueTable(competition.name, competition.currentSeason.display_name, at_date)
 
-    matches_until = MatchSchedule.objects.filter(Q(ms_time__lte=at_date) & Q(ms_league__exact=1))
+    matches_until = Match.objects.select_related('home_team', 'away_team').filter(date__lt=at_date)
     st.played_count = matches_until.count()
 
-    pl_teams = Team.objects.filter(Q(t_id__lte=20) & Q(t_id__gte=1))
-    st.teams_count = pl_teams.count()
+    teams = competition.currentSeason.teams.all()
+    st.teams_count = teams.count()
 
     # init dict of score table and teams
     temp_score_table = {}
-    for id in range(1, 21):
-        temp_score_table[id] = LeagueTableRow(id)
+    for team in teams:
+        temp_score_table[team.id] = LeagueTableRow(team.id, team.name)
 
     for match in matches_until:
-        home_id = match.ms_team1.t_id
-        away_id = match.ms_team2.t_id
+        if match.status != Match.FINISHED:
+            continue
 
-        if match.ms_team1_score > match.ms_team2_score:
+        home_id = match.home_team.id
+        away_id = match.away_team.id
+
+        # calculate point, won, drawn and lost
+        if match.home_score > match.away_score:
             # case home team win
             temp_score_table[home_id].point += 3
-        elif match.ms_team2_score > match.ms_team1_score:
+
+            temp_score_table[home_id].won += 1
+            temp_score_table[away_id].lost += 1
+        elif match.away_score > match.home_score:
             # case away team win
             temp_score_table[away_id].point += 3
+
+            temp_score_table[away_id].won += 1
+            temp_score_table[home_id].lost += 1
         else:
-            # case equal
+            # case draw
             temp_score_table[home_id].point += 1
             temp_score_table[away_id].point += 1
-        
-        # calculate goals
-        temp_score_table[home_id].goals += match.ms_team1_score - match.ms_team2_score
-        temp_score_table[away_id].goals += match.ms_team2_score - match.ms_team1_score
 
-    # add team name to table
-    for team_id in temp_score_table.keys():
-        team = Team.objects.get(pk=team_id)
-        temp_score_table[team_id].team_name = team.t_shortname
+            temp_score_table[home_id].drawn += 1
+            temp_score_table[away_id].drawn += 1
 
-    return sorted(temp_score_table.values(), key=lambda d: (d.point, d.goals), reverse=True)
+        # increase played field
+        temp_score_table[home_id].played += 1
+        temp_score_table[away_id].played += 1
+
+        # goal for and goal against
+        temp_score_table[home_id].goal_for += match.home_score
+        temp_score_table[home_id].goal_against += match.away_score
+
+        temp_score_table[away_id].goal_for += match.away_score
+        temp_score_table[away_id].goal_against += match.home_score
+
+        # calculate goal difference
+        temp_score_table[home_id].goal_difference += match.home_score - match.away_score
+        temp_score_table[away_id].goal_difference += match.away_score - match.home_score
+
+    return sorted(temp_score_table.values(), key=lambda d: (d.point, d.goal_difference), reverse=True)
--- scorebasedrecommend/views.py
+++ scorebasedrecommend/views.py
@@ -1,13 +1,18 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.utils import timezone
 from .leaguetable import get_score_table
-from datetime import date
+from website.models import Competition
+
 
 def index(request):
     return HttpResponse('score based recommendation')
 
+
 def test(request):
-    st = get_score_table('premier league', date(2018, 6, 1))
+    premier_league = Competition.objects.select_related('currentSeason').get(id=2021)
+    st = get_score_table(premier_league, timezone.now())
     context = {
         'teams_count': len(st),
         'league_table': st
