49a0b70e3caf4ef3576f748c05ca785209917b72
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-12-17 09:31:55 +0700

    Update RecommendedMatch model to support rule based recommendation

--- core/models.py
+++ core/models.py
@@ -7,13 +7,15 @@ class RecommendedMatch(models.Model):
     CONTENTBASED = 'CONTENTBASED'
     COLLABORATIVE = 'COLLABORATIVE'
     HYBRID = 'HYBRID'
+    RULEBASED = 'RULEBASED'
     RECOMMENDATION_TYPE = (
         (CONTENTBASED, 'Content-based'),
         (COLLABORATIVE, 'Collaborative'),
+        (RULEBASED, 'Rule-based'),
         (HYBRID, 'Hybrid')
     )
 
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='recommended_matches')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='recommended_matches', null=True, blank=True)
     match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='+')
     recommendatoin_type = models.CharField(max_length=13, choices=RECOMMENDATION_TYPE, default=CONTENTBASED)
     value = models.FloatField()
